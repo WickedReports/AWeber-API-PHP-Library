9e48e1ba390cb7fa8b3699fb29797a0555aa59df
Yevgenii Ostroushko <ostrojen@wickedreports.com>
2023-07-20 16:29:04 +0300

    use psr message class to parse response

--- aweber_api/curl_response.php
+++ aweber_api/curl_response.php
@@ -11,6 +11,8 @@
 # about the libcurl extension for PHP -- http://github.com/shuber/curl/tree/master
 #
 
+use GuzzleHttp\Psr7\Message;
+
 class CurlResponse
 {
     public $body = '';
@@ -18,31 +20,18 @@ class CurlResponse
 
     public function __construct($response)
     {
-        # Extract headers from response
-        $pattern = '#HTTP/\d\.\d.*?$.*?\r\n\r\n#ims';
-        preg_match_all($pattern, $response, $matches);
-        $headers = explode("\r\n", str_replace("\r\n\r\n", '', array_pop($matches[0])));
+        $response = Message::parseResponse($response);
+        $this->headers = $response->getHeaders();
+        $this->headers['Http-Version'] = $response->getProtocolVersion();
+        $this->headers['Status-Code'] = $response->getStatusCode();
+        $this->headers['Status'] = $response->getReasonPhrase();
 
-        # Extract the version and status from the first header
-        $version_and_status = array_shift($headers);
-        preg_match('#HTTP/(\d\.\d)\s(\d\d\d)\s(.*)#', $version_and_status, $matches);
-        $this->headers['Http-Version'] = $matches[1];
-        $this->headers['Status-Code'] = $matches[2];
-        $this->headers['Status'] = $matches[2].' '.$matches[3];
-
-        # Convert headers into an associative array
-        foreach ($headers as $header) {
-            preg_match('#(.*?)\:\s(.*)#', $header, $matches);
-            $this->headers[$matches[1]] = $matches[2];
-        }
-
-        # Remove the headers from the response body
-        $this->body = preg_replace($pattern, '', $response);
+        $this->body = $response->getBody();
     }
 
     public function __toString()
     {
-        return $this->body;
+        return $this->body->getContents();
     }
 
     public function headers(){
